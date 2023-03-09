7fa501dc4cda1d585c47ba50736935b60f8bcc93
trongqle <trong.q.le@sjsu.edu>
2023-03-08 19:45:59 -0800

    first commit

---
+++ Flixster1/MovieCell.swift
@@ -0,0 +1,40 @@
+//
+//  MovieCell.swift
+//  Flixster1
+//
+//  Created by LE, TRONG QUOC on 3/8/23.
+//
+
+import UIKit
+import Nuke
+
+class MovieCell: UITableViewCell {
+    
+    
+    
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var movieNameLabel: UILabel!
+    @IBOutlet weak var movieDescLabel: UILabel!
+    
+    
+    /// Configures the cell's UI for the given track.
+    func configure(with movie: Movie) {
+        movieNameLabel.text = movie.mName
+        movieDescLabel.text = movie.mMovieDesc
+
+        // Load image async via Nuke library image loading helper method
+        Nuke.loadImage(with: movie.artworkUrl100, into: movieImageView)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
